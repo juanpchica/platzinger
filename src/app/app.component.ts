97239800df344e5b43eaef86a171b76c63a78597
juanpchcia <juanpchica@hotmail.com>
2019-02-09 17:20:26 -0500

    Navegación principal de la app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,39 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component , ViewChild} from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+    
+    //Para poder agregar la navegacion aca
+    @ViewChild(Nav) nav: Nav;
+    rootPage:any = 'HomePage';
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
-  }
+    //Arreglo con las paginas y titulos y nombre del componente para usar en el menu
+    pages: Array<{title:string,component:any}>;
+
+
+    constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+        platform.ready().then(() => {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            statusBar.styleDefault();
+            splashScreen.hide();
+        });
+    
+        this.pages = [
+            {title:"Home",component:"HomePage"},
+            {title:"About",component:"AboutPage"},
+            {title:"Profile",component:"ProfilePage"}
+        ];
+    }
+
+    openPage(page){
+        //Abro una pagina nueva establecida como root
+        this.nav.setRoot(page);
+    }
 }
 
